9b21185a61c8f8b554ad8820b291fe423a792148
HubertR <hubertcfc@gmail.com>
2020-10-07 17:59:45 +0200

    ZadanieJava

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+package main.java;
+import static java.lang.Math.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*
+        Scanner scanner = new Scanner(System.in);
+        int liczba = 0;
+        int second = 2;
+        System.out.println("Podaj liczbę: ");
+        liczba = scanner.nextInt();
+        for (int i = 2; i <= liczba; pow(i, second)) {
+            System.out.println(i);
+        }
+*/
+
+        Scanner scan = new Scanner(System.in);
+        int i;
+        int a=0;
+        int b=0;
+        System.out.println("Podaj liczbę: ");
+        if(b >= 0);
+        for(i = 0; i < scan.nextInt() ; i++){
+            b = scan.nextInt();
+            a+=b;
+
+        }
+        System.out.println("Suma liczb to :" + a);
+
+
+        }
+        }
+
+
+
+
+
+
